35386d4682fb736e358f759b86042eb329f31ebc
Sergeantwinter <aryan.mahamune02@gmail.com>
2023-01-14 14:30:36 +0530

    v1

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+
+let mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/task');
+
+let schema = mongoose.Schema({
+  
+  content: {
+    type: String,
+    required: true
+  }
+
+})
+
+module.exports = mongoose.model('tasks', schema);
